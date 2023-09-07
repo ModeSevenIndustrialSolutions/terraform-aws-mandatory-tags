9655d80d8fe15cb22f45883d69eeee4ecdd0023e
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-09-07 16:10:42 +0100

    Chore: update provider dependency
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- provider.tf
+++ provider.tf
@@ -5,3 +5,10 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  # Dummy provider to prevent cyclic dependency
+  # See: https://github.com/hashicorp/terraform-provider-aws/issues/28772
+  region = "us-west-2"
+  alias  = "bootstrap"
+}
