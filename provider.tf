cde220ae6dca34b74cbbd6737b3d157c56ee2a2e
Matthew Watkins <mwatkins@linuxfoundation.org>
2023-09-08 13:57:05 +0100

    Fix: Remove faulty AWS provider
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- provider.tf
+++ provider.tf
@@ -5,10 +5,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  # Dummy provider to prevent cyclic dependency
-  # See: https://github.com/hashicorp/terraform-provider-aws/issues/28772
-  region = "us-west-2"
-  alias  = "bootstrap"
-}
